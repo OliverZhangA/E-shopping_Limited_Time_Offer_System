dbd016aa67fa11830008e8a12988ee884d046cc2
Aohua <az147@duke.edu>
2022-07-16 22:21:21 -0400

    finish basic functions before jmeter pressure test

---
+++ generator/src/main/java/com/xxxx/seckill/mapper/OrderMapper.java
@@ -0,0 +1,16 @@
+package com.xxxx.seckill.mapper;
+
+import com.xxxx.seckill.pojo.Order;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author zhoubin
+ * @since 2022-07-16
+ */
+public interface OrderMapper extends BaseMapper<Order> {
+
+}
